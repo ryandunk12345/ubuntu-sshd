422d5091d1b5778516fff9bb1d68f4dbfe4f9d71
root <root@raspberry-pi-4b-1.lan>
2020-12-27 01:40:08 +0000

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-ssh-keygen -q -t rsa -N '' <<< ""$'\n'"y" 2>&1 >/dev/null
-
-#ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-#ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
-#ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
+ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
+ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
 
 mkdir -p /root/.ssh
 touch /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
-
-cat /etc/ssh/sshd_config
 
 /usr/sbin/sshd -D
